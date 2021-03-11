0c4c9f099912f6455a3e4be74567e0b0d65a19b9
周润锴 <zhourunkai@zhourunkaideMac.local>
2021-03-11 10:00:32 +0800

    feat(js): this

---
+++ learn-js/this/默认绑定.js
@@ -0,0 +1,40 @@
+/**
+ * var声明的变量 和 function 会默认变成全局对象window下的变量和方法
+ * 在严格模式下不会
+ */
+
+// "use strict";
+
+function aaa() {
+  function bbb() {
+    function ccc() {
+      function ddd() {
+        console.log(this);
+      }
+      ddd();
+    }
+    ccc();
+  }
+  bbb();
+}
+
+var a = 10;
+function foo() {
+  console.log(this.a);
+}
+foo();
+
+console.log(window.foo); // foo {}
+console.log(window.a); // 10
+
+// 函数内部的this指向window
+// this.a => window.a => 1
+// 因为函数内部有自己的作用域 var a = 2 不会修改window下的a
+var a = 1;
+function bar() {
+  var a = 2;
+  console.log(this); // window{}
+  console.log(this.a); // 1
+}
+
+bar();
